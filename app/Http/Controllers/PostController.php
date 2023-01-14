37ec135c74b672452fc821d51398f77e532ce3a7
hrtsgym <hrt_a_1206@yahoo.co.jp>
2023-01-14 22:20:40 +0900

    fifth 1 commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,4 +18,8 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
         //'post'はbladeファイルで使う変数。中⾝は$postはid=1のPostインスタンス。
     }
+    public function create()
+    {
+        return view('posts/create');
+    }
 }
